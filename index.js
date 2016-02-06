cd316066ce567b819876a0241337eea336a0bba1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-02-05 19:10:29 -0800

    update course list and setup for spring

--- index.js
+++ index.js
@@ -5,26 +5,31 @@ let scrape = require('scrape-uci')
 
 // List of ICS courses that I still need to graduate
 let needed = [
-  'INF 113',
-  'INF 131',
-  'INF 151',
+  'ICS 045J',
   'INF 161',
   'INF 191A',
   'INF 191B',
   'INF 132',
+
+  // 3 courses from this list
   'INF 133',
   'INF 143',
+  'INF 153',
+  'INF 162',
   'INF 162W',
+  'INF 171',
+
+  // 2 project courses
+  'INF 125',
+  'INF 134',
   'INF 148',
   'INF 163',
-  'INF 134',
-  'INF 125',
-  'INF 141',
+  'INF 172',
 ]
 
 // Request list of courses offered next quarter 
 scrape({
-  source:'ics', dept:'INF',
+  source:'ics', dept:'ALL',
   year:'2016', period:'spring'
 }, (err, canTake) => {
   if (err) throw err;
